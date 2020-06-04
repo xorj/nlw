6cf07a83877a28370adb1390f0b560ef4319f154
xorj <jorge.santos@dcomp.ufs.br>
2020-06-03 22:41:29 -0300

    Adding frist day files

---
+++ server/src/database/connection.ts
@@ -0,0 +1,20 @@
+import knex from "knex";
+import path from "path";
+
+const connection = knex({
+  client: "sqlite3",
+  connection: {
+    filename: path.resolve(__dirname, "database.sqlite"),
+  },
+});
+
+export default connection;
+
+//Migration é o histórico do banco de dados
+//create table points
+//create table users
+//create table points_users
+
+//Fundamental methods
+//UP - adiciona uma tabela
+//DOWN - apaga a última tabela
